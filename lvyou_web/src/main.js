8efd7b9f959f73b03c8af0efe4196076a92120d6
lijiasheng <2330165939@qq.com>
2022-09-06 09:52:32 +0800

    code

---
+++ lvyou_web/src/main.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import { postRequest } from "./utils/api";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import hevueImgPreview from "hevue-img-preview";
+import editor from "@blog1997/vue-umeditor";
+import * as echarts from "echarts";
+Vue.prototype.$echarts = echarts;
+Vue.use(editor);
+Vue.use(hevueImgPreview);
+Vue.use(ElementUI);
+
+// 配置全局属性
+Vue.prototype.postRequest = postRequest;
+
+Vue.config.productionTip = false;
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
