86cce13557d808e439f544e43ad71bcad026e1a0
Key <2330165939@qq.com>
2022-12-11 15:18:51 +0800

    feat：服务器路由以及write

--- node-server/server.js
+++ node-server/server.js
@@ -1,8 +1,20 @@
 // 引入http模块
 var http = require('http');
+// 引入URL模块
+var url = require('url');
 // 创建http服务
 var server = http.createServer(function (req, res) {
-  console.log('Lee welcome to node.js');
+  if (req.url == '/index?userId=123') {
+    console.log(req.url);
+    res.write('index');
+    res.end();
+    var urlobj = url.parse(req.url);
+    console.log(urlobj);
+  } else if (req.url == '/login') {
+    console.log(req.url);
+    res.write('login');
+    res.end();
+  }
 });
 // 监听端口号
 server.listen(8888, function () {
